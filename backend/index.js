a0a9e1d42a5138da2bd6cf8554ab4c6e88319cef
vekariya-jay <vekariyajay985@gmail.com>
2026-04-19 17:47:37 +0530

    add frontend url

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ console.log("🔥 SERVER FILE IS RUNNING hello");
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "bellavita-gray.vercel.app",
+  origin: [
+    "https://bellavita-gzsqrtza3-jkvekariyas-projects.vercel.app",
+     "http://localhost:5173"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
