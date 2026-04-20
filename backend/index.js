077d27233ea69f749b577acfe3906d77eb00f7ad
vekariya-jay <vekariyajay985@gmail.com>
2026-04-20 14:44:45 +0530

    backend change

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,7 @@ console.log("🔥 SERVER FILE IS RUNNING hello");
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: [
-    "https://bellavita-55va7mdx8-jkvekariyas-projects.vercel.app",
-     "http://localhost:5173"
-  ],
+  origin: true,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
